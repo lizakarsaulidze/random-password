8b243da1ea0de7e1e04fd3d237efad9c2c0d271b
lizakarsaulidze <115642902+lizakarsaulidze@users.noreply.github.com>
2024-05-09 13:22:23 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABSDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "absdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = ";'./,=+)(\|~~!@#$%^&*";
+
+const allCars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];  
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];  
+    password += number[Math.floor(Math.random() * number.length)];  
+    password += symbol[Math.floor(Math.random() * symbol.length)];  
+
+    while(length > password.length){
+        password += allCars[Math.floor(Math.random() * allCars.length)]; 
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+
